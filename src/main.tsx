f35fb64c835f6c401cff55cce91230faba5b1b5e
yannickRafael <yannickrafel286@gmail.com>
2024-10-21 21:56:05 +0200

    added configuration for translation

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,32 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomeView from "./views/HomeView.tsx";
 import PublishView from "./views/PublishView.tsx";
 import ProfileView from "./views/ProfileView.tsx";
+import i18next from "i18next";
+import globalPt from "./assets/translations/pt/global.json"
+import {I18nextProvider} from "react-i18next";
+import NavBar from "./components/NavBar.tsx";
+
+i18next.init({
+    interpolation: {escapeValue:false},
+    lng: "en",
+    resources:{
+        pt : {
+            global: globalPt
+        }
+    }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<HomeView/>}/>
-              <Route path="/publish" element={<PublishView/>} />
-              <Route path="/profile" element={<ProfileView/>}/>
-          </Routes>
-      </BrowserRouter>
+      <I18nextProvider i18n={i18next}>
+          <NavBar/>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<HomeView/>}/>
+                  <Route path="/publish" element={<PublishView/>} />
+                  <Route path="/profile" element={<ProfileView/>}/>
+              </Routes>
+          </BrowserRouter>
+      </I18nextProvider>
   </StrictMode>,
 )
