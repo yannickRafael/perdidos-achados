d98abc4ae6857c21ab88c8ff7aa797cf93bcbeb7
yannickRafael <yannickrafel286@gmail.com>
2024-10-21 22:15:55 +0200

    added navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,43 @@
-function NavBar() {
+import {useTranslation} from "react-i18next";
+import "../styles/Navbar.css"
+import {useState} from "react";
+
+function Navbar() {
+    const [t] = useTranslation("global");
+    const [menuOpen, setMenuOpen] = useState(false)
     return(
         <div>
+            <div className="nav-container">
+                <div className="nav-title">
+                    <img src="/public/logo.jpeg" alt=""/>
+                </div>
 
+                <nav>
+                    <ul className={menuOpen ? "open" : ""}>
+                        <li>
+                            <a href="/">{t("navbar.home")}</a>
+                        </li>
+
+                        <li>
+                            <a href="/profile">{t("navbar.profile")}</a>
+                        </li>
+
+                        <li>
+                            <a href="/publish">{t("navbar.publish")}</a>
+                        </li>
+
+                    </ul>
+                    <div className="menu" onClick={() => {
+                        setMenuOpen(!menuOpen)
+                    }}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </nav>
+            </div>
         </div>
     );
 }
 
-export default NavBar;
+export default Navbar;
